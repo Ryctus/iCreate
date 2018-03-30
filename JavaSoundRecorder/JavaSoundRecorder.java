a06a9254b625f061a1de72dfd69090d472c5c44e
Theo_Styblinski <theo.styblinski@etu.univ-nantes.fr>
2018-03-30 10:15:57 +0200

    ??

--- JavaSoundRecorder/JavaSoundRecorder.java
+++ JavaSoundRecorder/JavaSoundRecorder.java
@@ -59,43 +59,8 @@ public class JavaSoundRecorder {
 					System.out.println("\n");
 				}
 			}
-			// pour chaque mixer on va regarder ses caracteristiques
-			/*for (Mixer.Info a : mixerInfo) {
-
-				System.out.println("\n\n" + a + " Description 666 :" + a.getDescription());
-
-				System.out.println("--------------------------------");
-
-				Mixer mix = AudioSystem.getMixer(a);
-
-				// Visualisation dex controles supportés par chaque Mixer
-				for (Control c : mix.getControls()) {
-					System.out.println("Controls supported by Mixer:");
-					System.out.println("\t" + c);
-				}
-
-				// Visualisation des SourceLine disponibles pour ce Mixer
-				for (Line.Info i : mix.getSourceLineInfo()) {
-					System.out.println("\n" + i);
-
-					// Visualisation dex controles supportés par cette Line
-					System.out.println("Controls supported by Source Line:");
-					for (Control c : AudioSystem.getLine(i).getControls()) {
-						System.out.println("\t" + c);
-					}
-				}
-				// Visualisation des TargetLine disponibles pour ce Mixer
-				for (Line.Info i : mix.getTargetLineInfo()) {
-					System.out.println("\n" + i);
-
-					// Visualisation dex controles supportés par cette Line
-					System.out.println("Controls supported by Target Line:");
-					for (Control c : AudioSystem.getLine(i).getControls()) {
-						System.out.println("\t" + c);
-					}
-				}
-			}*/
-
+			
+			// Création de la dataLine
 			AudioFormat format = getAudioFormat();
 			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 			System.out.println(info);
